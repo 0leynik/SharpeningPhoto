5ca069b8520095c180379cc1138287454e0184f8
ShakeJumper <dimitr95@mail.ru>
2017-10-31 12:58:35 +0300

    commit msg

--- lmdb/lmdb_creator.py
+++ lmdb/lmdb_creator.py
@@ -1,62 +1,130 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy as np
-# import lmdb
-# import caffe
+import lmdb
+import caffe
 
-N = 133527 * 3  # mb+fb+mfb
+
+'''
+ГЕНЕРАЦИЯ ПЕРЕМЕШАННЫХ ИМЕН ИЗОБРАЖЕНИЙ
+'''
+
+ext = '.JPEG'
+
+
+def gen_shuffle_names(N):
+    print '\nGen shuffle names:'
+    img_names = []
+    for i in range(1, N+1):
+        i_str = str(i)
+
+        sh = i_str + '_sh' + ext
+        mb = i_str + '_mb' + ext
+        fb = i_str + '_fb' + ext
+        mfb = i_str + '_mfb' + ext
+
+        img_names.append([mb, sh])
+        img_names.append([fb, sh])
+        img_names.append([mfb, sh])
+
+    np_img_names = np.array(img_names)
+    print np_img_names
+    print np_img_names.shape
+
+    np.random.shuffle(np_img_names)
+    print np_img_names
+    print np_img_names.shape
+
+    # blur_img_names = np_img_names[:, 0]
+    # sharp_img_names = np_img_names[:, 1]
+    return np_img_names
+
+
+'''
+СОЗДАНИЕ LMDB
+'''
+
 const_w = 500
 const_h = 375
 
-# def create_lmdb():
-#
-#     # NumBytes = NumImages * shape[0] * shape[1] * shape[2] * sizeof(datatype) * sizeof(label)
-#     map_size = N * 3 * const_w * const_h * np.dtype(np.uint8).itemsize * np.dtype(np.int32).itemsize * 3
-#     # 300 435 750 000 bytes * (коэффициент 3 на всякий случай)
-#
-#     env = lmdb.open('mylmdb', map_size=map_size)
-#
-#     with env.begin(write=True) as txn:
-#         for i in range(N):
-#
-#             height, width, channels = img.shape
-#
-#             datum = caffe.proto.caffe_pb2.Datum()
-#
-#             datum.channels = channels
-#             datum.height = height
-#             datum.width = width
-#             # datum.data = img.tobytes()  # or .tostring() if numpy < 1.9
-#             datum.data = img.tostring()
-#             datum.label = int(0)
-#
-#             keystr = '{:08}'.format(i)
-#
-#             txn.put(keystr.encode('ascii'), datum.SerializeToString())
+
+def create_lmdb(db_name, img_folder, img_names, N):
+
+    # NumBytes = NumImages * 3(mb+fb+mfb) * shape[0] * shape[1] * shape[2] * sizeof(datatype) * sizeof(label)
+    map_size = N * 3 * 3 * const_h * const_w * np.dtype(np.uint8).itemsize * np.dtype(np.int32).itemsize * 3
+    # 300 435 750 000 bytes * (коэффициент 3 на всякий случай)
+
+    env = lmdb.open(db_name, map_size=map_size)
+    print 'LMDB \"' + db_name + '\" opened.\nStart writing!'
+
+    with env.begin(write=True) as txn:
+        for i in range(N):
+            print i
+            img = cv2.imread(img_folder + img_names[i])
+            # height, width, channels = img.shape
+
+            # HxWxC -> CxHxW
+            img = np.transpose(img, (2, 0, 1))
+            channels, height, width = img.shape
+
+            datum = caffe.proto.caffe_pb2.Datum()
+            datum.channels = channels
+            datum.height = height
+            datum.width = width
+            datum.data = img.tostring()
+            # datum.data = img.tobytes()  # or .tostring() if numpy < 1.9
+            datum.label = int(0)
+            key_str = '{:08}'.format(i)
+
+            txn.put(key_str.encode('ascii'), datum.SerializeToString())
+    print 'Writing to \"' + db_name + '\" done!'
+
+    env.close()
+    print 'LMDB \"' + db_name + '\" closed.'
 
 
 if __name__ == '__main__':
 
-    img = cv2.imread('8.JPEG');
-    height, width = img.shape[:2]
+    print 'train'
+    N = 133527
+    names = gen_shuffle_names(N)
+    img_folder = '/home/doleinik/SharpeningPhoto/quality_ImageNet/train_500/images/'
 
+    create_lmdb('train_blur_lmdb',
+                img_folder,
+                names[:, 0],
+                N)
+    create_lmdb('train_sharp_lmdb',
+                img_folder,
+                names[:, 1],
+                N)
 
-    # поворот наибольшей стороной по горизонтали
-    if height > width:
-        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        height, width = img.shape[:2]
+    print 'test'
+    N = 11853
+    names = gen_shuffle_names(N)
+    img_folder = '/home/doleinik/SharpeningPhoto/quality_ImageNet/test_500/images/'
 
+    create_lmdb('test_blur_lmdb',
+                img_folder,
+                names[:, 0],
+                N)
+    create_lmdb('test_sharp_lmdb',
+                img_folder,
+                names[:, 1],
+                N)
 
-    if height >= const_h:
-        img = img[:const_h, :const_w]
-    else:
-        img = img[:, :const_w]
+    print 'val'
+    N = 5936
+    names = gen_shuffle_names(N)
+    img_folder = '/home/doleinik/SharpeningPhoto/quality_ImageNet/val_500/images/'
 
-        bottom_size = const_h - height
-        img = cv2.copyMakeBorder(img, 0, bottom_size, 0, 0, cv2.BORDER_REFLECT_101)
+    create_lmdb('val_blur_lmdb',
+                img_folder,
+                names[:, 0],
+                N)
+    create_lmdb('val_sharp_lmdb',
+                img_folder,
+                names[:, 1],
+                N)
 
-
-    print img.shape
-
-    cv2.imshow('img',img)
-    cv2.waitKey()
+    print '\nComplete!'
--- lmdb/lmdb_reader.py
+++ lmdb/lmdb_reader.py
@@ -11,40 +11,38 @@ import cv2
 # http://research.beenfrog.com/code/2015/12/30/write-read-lmdb-example.html
 # http://shengshuyang.github.io/hook-up-lmdb-with-caffe-in-python.html
 
-visualize = True
+visualize = False
 lmdb_path = "/home/doleinik/l4/lmdb/train_lmdb"
 
 env = lmdb.open(lmdb_path)
 with env.begin() as txn:
+    with txn.cursor() as curs:
+        datum = caffe.proto.caffe_pb2.Datum()
 
-    cursor = txn.cursor()
-    datum = caffe.proto.caffe_pb2.Datum()
+        for key, value in curs:
 
-    for key, value in cursor:
+            datum.ParseFromString(value)
 
-        datum.ParseFromString(value)
+            label = datum.label
+            data = caffe.io.datum_to_array(datum)
+            # (datum.channels, datum.height, datum.width)
 
-        label = datum.label
-        data = caffe.io.datum_to_array(datum)
-        # (datum.channels, datum.height, datum.width)
+            # if label == 999:
+            print "key: ", key
+            # print "value ", value
+            print "datum.label ", label
+            # print "datum.data ", data
+            # print type(data)
+            # print data.shape
+            # print data.dtype
 
-        # if label == 999:
-        print "key: ", key
-        print "value ", value
-        print "datum.label ", label
-        print "datum.data ", data
-        # print type(data)
-        # print data.shape
-        # print data.dtype
+            if visualize:
+                # CxHxW -> HxWxC
+                img = np.transpose(data, (1, 2, 0))
+                # BGR -> RGB
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-
-        if visualize:
-            # CxHxW -> HxWxC
-            img = np.transpose(data, (1, 2, 0))
-            # BGR -> RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            # matplotlib.pyplot.imshow()
-            # HxWx3 – RGB (float or uint8 array)
-            plt.imshow(img)
-            plt.show()
+                # matplotlib.pyplot.imshow()
+                # HxWx3 – RGB (float or uint8 array)
+                plt.imshow(img)
+                plt.show()

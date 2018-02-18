2bc3aef517bbd2e126c2a490535dd2e6ae91100e
ShakeJumper <dimitr95@mail.ru>
2018-02-18 22:32:29 +0300

    commit msg

--- models/SP_evaluate.py
+++ models/SP_evaluate.py
@@ -22,21 +22,29 @@ import SP_model
 
 if __name__ == '__main__':
 
+    metrics = np.loadtxt("~/SP_metrics.csv", delimiter=",")
 
-
-
-    model = SP_model.get_unet_128()
-    model.load_weights('lung.h5')
-
-    plt.plot(hist.history['loss'], color='b')
-    plt.plot(hist.history['val_loss'], color='r')
+    plt.figure('loss')
+    plt.plot(metrics[:, 0])
     plt.show()
-    plt.plot(hist.history['dice_coef'], color='b')
-    plt.plot(hist.history['val_dice_coef'], color='r')
+    plt.figure('acc')
+    plt.plot(metrics[:, 1])
     plt.show()
 
-    y_hat = model.predict(x_val)
-    fig, ax = plt.subplots(1,3,figsize=(12,6))
-    ax[0].imshow(x_val[0,:,:,0], cmap='gray')
-    ax[1].imshow(y_val[0,:,:,0])
-    ax[2].imshow(y_hat[0,:,:,0])
+
+    # model = SP_model.get_unet_128()
+    # model.load_weights('lung.h5')
+    #
+    # plt.plot(hist.history['loss'], color='b')
+    # plt.plot(hist.history['val_loss'], color='r')
+    # plt.show()
+    # plt.plot(hist.history['dice_coef'], color='b')
+    # plt.plot(hist.history['val_dice_coef'], color='r')
+    # plt.show()
+    #
+    # y_hat = model.predict(x_val)
+    # fig, ax = plt.subplots(1,3,figsize=(12,6))
+    # ax[0].imshow(x_val[0,:,:,0], cmap='gray')
+    # ax[1].imshow(y_val[0,:,:,0])
+    # ax[2].imshow(y_hat[0,:,:,0])
+

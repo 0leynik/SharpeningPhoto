a1017ffafaab435ccd63b84ac4f1f09b0604d321
ShakeJumper <dimitr95@mail.ru>
2018-05-15 15:54:17 +0300

    commit msg

--- models/SP_evaluate.py
+++ models/SP_evaluate.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["DISPLAY"] = "localhost:12.0"
 
 import glob
 import numpy as np

d5f2913b08c681511eed597fbb5d2613aa9b395e
ShakeJumper <dimitr95@mail.ru>
2018-07-05 16:00:56 +0300

    commit msg

--- models/train.py
+++ models/train.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from keras.models import Model
-from keras.layers import Conv2D, Input
+from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, Concatenate
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
 import numpy as np
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import h5py
 
-def create_model():
+def get_srcnn_model():
     inputs = Input(shape=(None, None, 1))
     conv = Conv2D(128, (9, 9), padding='same', activation='relu')(inputs)
     conv = Conv2D(64, (3, 3), padding='same', activation='relu')(conv)
@@ -19,6 +19,63 @@ def create_model():
 
     model = Model(inputs=[inputs], outputs=[outputs])
     # model.compile(optimizer=Adam(lr=0.0003), loss='mean_squared_error')
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    return model
+
+def get_unet_model():
+
+    img_shape = (None, None, 1)
+
+    inputs = Input(shape=img_shape)
+
+    #64
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    # 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    # 16
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    # 8
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    # 4
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    deconv6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), activation='relu', padding='same')(conv5)
+    up6 = Concatenate(axis=-1)([deconv6, conv4])
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    deconv7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), activation='relu', padding='same')(conv6)
+    up7 = Concatenate(axis=-1)([deconv7, conv3])
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    deconv8 = Conv2DTranspose(64, (2, 2), strides=(2, 2), activation='relu', padding='same')(conv7)
+    up8 = Concatenate(axis=-1)([deconv8, conv2])
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    deconv9 = Conv2DTranspose(32, (2, 2), strides=(2, 2), activation='relu', padding='same')(conv8)
+    up9 = Concatenate(axis=-1)([deconv9, conv1])
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
     model.compile(optimizer='adam', loss='mean_squared_error')
 
     return model
@@ -31,7 +88,8 @@ if not os.path.isdir(loss_save_dir):
 
 def train():
 
-    model = create_model()
+    # model = get_srcnn_model()
+    model = get_unet_model()
     model.summary()
 
     f_train = h5py.File('../dataset/train.h5', 'r')
@@ -125,5 +183,5 @@ def plot_graph():
     plt.savefig(os.path.join(loss_save_dir, 'graph_history_loss.png'))
 
 if __name__ == "__main__":
-    # train()
-    plot_graph()
+    train()
+    # plot_graph()
